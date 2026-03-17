f3b2281234f8859e56354375532c3b7195911143
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2026-03-16 20:09:12 -0500

    [feat]: test setup

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+};
+
+export default config;
